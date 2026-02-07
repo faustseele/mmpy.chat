162f991b86b67d0774c72dc7d7cb29cb767d73a7
faustseele <etevve@gmail.com>
2026-02-07 18:59:16 +0700

    implements vite-manifest, adds offline support

--- vite.config.js
+++ vite.config.js
@@ -24,19 +24,19 @@ export default defineConfig({
             src: "/logo.ico",
             sizes: "any",
             type: "image/x-icon",
-            purpose: "any",
+            purpose: "any maskable",
           },
           {
             src: "/icon-192.png",
             sizes: "192x192",
             type: "image/png",
-            purpose: "any",
+            purpose: "any maskable",
           },
           {
             src: "/icon-512.png",
             sizes: "512x512",
             type: "image/png",
-            purpose: "any",
+            purpose: "any maskable",
           },
         ],
         screenshots: [
