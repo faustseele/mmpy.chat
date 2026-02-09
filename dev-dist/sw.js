8a52691a59072e305258c01efb5b0162c563cc36
faustseele <etevve@gmail.com>
2026-02-09 14:16:01 +0700

    emilinates font-family boilerplate

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1jl15nsc8do"
+    "revision": "0.0sd660ipqd"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
