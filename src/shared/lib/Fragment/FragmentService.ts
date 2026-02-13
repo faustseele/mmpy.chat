2b0de1b9e9f03aedc198dbb039bf6e49f8ea5865
faustseele <etevve@gmail.com>
2026-02-13 16:39:54 +0700

    improves fragment-service, lighter on stub replacement

--- src/shared/lib/Fragment/FragmentService.ts
+++ src/shared/lib/Fragment/FragmentService.ts
@@ -1,7 +1,6 @@
 import Handlebars from "handlebars";
-import { BaseConfigs, BaseProps } from "../Component/model/base.types.ts";
+import { BaseConfigs } from "../Component/model/base.types.ts";
 import { ChildGraph } from "../Component/model/children.types.ts";
-import { ComponentNode } from "../Component/model/types.ts";
 
 /**
  * @FragmentService – stateless feature-service.
@@ -10,7 +9,7 @@ import { ComponentNode } from "../Component/model/types.ts";
  * * * before attaching them to the DOM
  * @sourceMarkup – raw HTML-string
  * e.g., sourceMarkup provided by Component
- * @compiledSourceMarkup – Handlebars-compiled HTML-string with placeholders
+ * @compiledSourceMarkup – Handlebars-compiled HTML-string with stubs
  * @returns DocumentFragment
  * DocumentFragment is an off-DOM lightweight container.
  * when appended to a DOM Element, its child nodes-
@@ -18,7 +17,6 @@ import { ComponentNode } from "../Component/model/types.ts";
  */
 export default class FragmentService<C extends BaseConfigs> {
   private templateCache = new Map<string, HandlebarsTemplateDelegate>();
-  private _template = document.createElement("template");
 
   public compile(sourceMarkup: string, configs: C): DocumentFragment {
     let template = this.templateCache.get(sourceMarkup);
@@ -40,8 +38,8 @@ export default class FragmentService<C extends BaseConfigs> {
   ): DocumentFragment {
     let template = this.templateCache.get(sourceMarkup);
 
-    /* creates <li id="random UUID"></li>.. placeholders for children */
-    const divPlaceholders = this._createDivPlaceholders(children);
+    /* creates <li id="random UUID"></li>.. stubs for children */
+    const divStubs = this._createStubs(children);
 
     /* compile if not cached */
     if (!template) {
@@ -51,23 +49,23 @@ export default class FragmentService<C extends BaseConfigs> {
 
     /**
      * handles {{expressions-configs}} & {{{children-html expressions}}}
-     * inserts placeholders into the (cached) template
+     * inserts stubs into the (cached) template
      */
-    const compiledSourceMarkupWithPlaceholders = template({
+    const compiledSourceMarkupWithStubs = template({
       /* {{{children-html expressions}}} */
-      ...divPlaceholders,
+      ...divStubs,
       /* {{config-expressions}} of a parent */
       ...configs,
     });
 
-    /* creates a DocumentFragment <li> children placeholders */
-    const fragmentWithPlaceholders = this._createFragmentFromString(
-      compiledSourceMarkupWithPlaceholders,
+    /* creates a DocumentFragment <li> children stubs */
+    const fragmentWithStubs = this._createFragmentFromString(
+      compiledSourceMarkupWithStubs,
     );
 
-    /* swappes <li> children placeholders with corresponding Elements*/
-    const fragmentWithElements = this._replacePlaceholdersInFragment(
-      fragmentWithPlaceholders,
+    /* swappes <li> children stubs with corresponding Elements*/
+    const fragmentWithElements = this._replaceStubsInFragment(
+      fragmentWithStubs,
       children,
     );
 
@@ -75,82 +73,61 @@ export default class FragmentService<C extends BaseConfigs> {
   }
 
   /**
-   * generates an obj with either one li-placeholder '<tag></tag>' by key
-   * or a concatenated list of li-placeholders <tags> by key.
+   * generates an obj with either one li-stub '<tag></tag>' by key
+   * or a concatenated list of li-stubs <tags> by key.
    */
-  private _createDivPlaceholders(graph: ChildGraph): Record<string, string> {
-    const divPlaceholders: Record<string, string> = {};
+  private _createStubs(graph: ChildGraph): Record<string, string> {
+    const divStubs: Record<string, string> = {};
 
     Object.keys(graph.edges).forEach((edge) => {
       if (Array.isArray(graph.edges[edge])) {
-        /* sets placeholder for each child -> placeholdersList[] */
-        const placeholdersList = graph.edges[edge].map(
+        /* sets stub for each child -> stubsList[] */
+        const stubsList = graph.edges[edge].map(
           (id) => `<li data-id="${id}"></li>`,
         );
-        /* concatenates placeholdersList[] into one string */
-        divPlaceholders[edge] = placeholdersList.join("");
+        /* concatenates stubsList[] into one string */
+        divStubs[edge] = stubsList.join("");
       } else {
-        divPlaceholders[edge] = `<li data-id="${edge}"></li>`;
+        divStubs[edge] = `<li data-id="${edge}"></li>`;
       }
     });
 
-    return divPlaceholders;
+    return divStubs;
   }
 
   /**
    * Creates a DocumentFragment from a raw HTML string.
    */
   private _createFragmentFromString(htmlString: string): DocumentFragment {
-    /* sets the HTML of the inert template */
-    this._template.innerHTML = htmlString;
-
-    /* a deep clone of the template's content (DocumentFragment) */
-    return this._template.content.cloneNode(true) as DocumentFragment;
+    return document.createRange().createContextualFragment(htmlString);
   }
 
   /**
-   * iterates through the fragment and replaces all placeholder divs
+   * iterates through the fragment and replaces all stub <li>'s
    * with their corresponding, real component elements.
    */
-  private _replacePlaceholdersInFragment(
+  private _replaceStubsInFragment(
     fragment: DocumentFragment,
     graph: ChildGraph,
   ): DocumentFragment {
-    Object.keys(graph.edges).forEach((edge) => {
-      if (Array.isArray(graph.edges[edge])) {
-        graph.edges[edge].forEach((nodeId) => {
-          findAndReplace(graph.nodes[nodeId]);
-        });
-      } else {
-        findAndReplace(graph.nodes[edge]);
-      }
+    /* finds all stubs in one pass */
+    const stubs = fragment.querySelectorAll("li[data-id]");
+    const stubsMap = new Map<string, Element>();
+
+    stubs.forEach((stub) => {
+      stubsMap.set(stub.getAttribute("data-id") || "", stub);
     });
 
-    function findAndReplace(node: ComponentNode<BaseProps>) {
-      if (!node?.runtime?.instance) {
-        console.error("Child has no instance", node);
-        return;
-      }
-
-      const placeholder = fragment.querySelector(
-        `[data-id="${node.params.configs.id}"]`,
-      );
+    /* iterates children and swaps using the Map */
+    Object.values(graph.nodes).forEach((node) => {
+      const id = node.params.configs.id;
+      const stub = stubsMap.get(id);
       const childElement = node.runtime?.instance.element;
 
-      if (placeholder && childElement) {
-        placeholder.replaceWith(childElement);
-      } else {
-        /* console.error(
-            `FragmentService Error: Could not replace child placeholder.
-          Child ID: ${node.params.configs.id}.
-          Placeholder found: ${!!placeholder}.
-          Child element obtained: ${!!childElement}.
-          DocumentFragment content:`,
-            JSON.stringify(fragment),
-          ); */
+      if (stub && childElement) {
+        stub.replaceWith(childElement);
       }
-    }
-
+    });
     return fragment;
   }
 }
