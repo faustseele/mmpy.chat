d89a416b38d0527aa65b3fc0ec2b343a4370a7a4
faustseele <etevve@gmail.com>
2026-03-20 13:51:32 +0300

    revers back to 36acb5bde3bcbbed4b7004c38c6ed177fecb15a5 to check if netlify page gets revived

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.mh6fq76in3k"
+    "revision": "0.i12ab6jj6q4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
